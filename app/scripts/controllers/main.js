c8ee632bdb1b3428528766e58c4dd2ca640fb4be
paulbarrett79 <paulbarrett79@gmail.com>
2016-05-18 18:04:36 +0100

    progress on wednesday

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the gameoflifeChallengeApp
  */
 angular.module('gameoflifeChallengeApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $filter, $interval) {
     
 /*  	Given the Game of Life web page (as described above)
 When I set a seed pattern and click the start button
@@ -17,31 +17,114 @@ Then the grid will progressively step forward in time with the grid showing the 
 	//the controller controls:
 	//(the number of the rows and columns) - no, we're just using numbers for the row and column coordinates
 	//listening for the button press
+	var stepper;
 	$scope.buttonPressed = function(){
-
+		
+		stepper = $interval($scope.triggerBroadcast(), 1000);
 
 	};
 	//validating the fact that at least one tick has been created
 	//this can also be used to check that the population has finished and stop the recursion.
 	$scope.population = [];
-	$scope.hasAtLeastOneTickBeenCreated = function(){
-		//
-	};
+	
 
 	$scope.changePop = function(item, addOrRemove) {
-		console.log('changePop');
-		if(addOrRemove == false){
+
+		if(addOrRemove == true){
 			$scope.population.push(item);
 		}
 		else {
-			$scope.population = $filter("filter")($scope.population, item, true);
+			var index = $scope.population.indexOf(item);
+		    if (index != -1) {
+		        $scope.population.splice(index, 1);
+		    }
 		}
 	}
+
+	$scope.triggerBroadcast = function(){
+		if($scope.population.length > 0){
+		var newPopulation = [];
+		var frozenPopulation = angular.copy($scope.population);
+		angular.forEach($scope.rows, function(r){
+
+			angular.forEach($scope.columns, function(c){
+
+				var neighbours = [];
+				var current = {}
+				var isCurrentAlive = false;
+
+				for (var i = r - 1; i >= r + 1; i++) {
+	  				for (var j = c - 1; j >= c + 1; j++) {
+	      				 if(i != r && j != c){
+	      				 	neighbours.push({
+		      				 	'row': i,
+		      				 	'column': j
+		      				 });
+	      				 }
+	      				 else {
+	      				 	current = {
+	      				 		'row': i,
+		      				 	'column': j
+	      				 	}
+	      				 }
+
+	      				 
+	      			};
+	  			};
+	  			//var myEl = angular.element( document.querySelector( '#' + r + c ) );
+	  			var idStr = r.toString() + c.toString();
+	  			//console.log(idStr);
+	  			var myEl = document.getElementById(idStr);
+	  			
+	  			if(angular.element(myEl).hasClass('alive')){
+	  				isCurrentAlive = true;
+	  			}
+
+	  			var intersect = _.intersection(neighbours, frozenPopulation);
+	  			
+	  			console.log('intersect',intersect );
+	  			//intersect not working for objects
+
+
+	  			//Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+				//Any live cell with two or three live neighbours lives on to the next generation.
+				//Any live cell with more than three live neighbours dies, as if by over-population.
+				//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+	  			if(intersect.length > 0){
+
+					if((intersect.length < 2 || intersect.length > 3) && isCurrentAlive){
+						myEl.click();
+					}
+					else if(intersect.length == 2 || intersect.length == 3){
+						//click myEl
+						myEl.click()
+					}
+					else if(intersect.length == 3 && !isCurrentAlive) {
+						//click myEl
+						myEl.click();	
+					}
+				}
+
+
+			});
+
+		});
+
+		//$scope.population = newPopulation;
+
+	}
+		
+		else {
+			$interval.cancel();
+		}
+	}
+
 	//the timer and stepping through the process
 	//recursive method?
 
 		//broadcasting the trigger for starting the examination of surrounding cells
-		$scope.$broadcast('checkSurroundingCells');
+		//$scope.$broadcast('checkSurroundingCells');
 
 	//there will be a directive for each cell which handles the .on() from the broadcast
 	$scope.columns = [];
@@ -51,5 +134,7 @@ Then the grid will progressively step forward in time with the grid showing the 
         $scope.columns.push(i);
 		$scope.rows.push(i);
 		i++;
-    }
+    };
+
+
   });
--- app/scripts/directives/singlecell.js
+++ app/scripts/directives/singlecell.js
@@ -9,7 +9,7 @@
 angular.module('gameoflifeChallengeApp')
   .directive('singlecell', function () {
     return {
-      template: '<div ng-class="statusClass" ng-click="changeStatus()">&nbsp;</div>',
+      template: '<div ng-class="statusClass" ng-click="changeStatus()" id="{{idString}}">&nbsp;</div>',
       restrict: 'E',
       /*link: function postLink(scope, element, attrs) {
         element.text('this is the singlecell directive');
@@ -18,36 +18,74 @@ angular.module('gameoflifeChallengeApp')
       	population: "=",
       	row: "=",
       	column: "=",
-      	changefunc: "&",
+      	changeFunc: "&",
       },
-      transclude: true,
       controller: function($scope, $filter){
       	
-      	//Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-		//Any live cell with two or three live neighbours lives on to the next generation.
-		//Any live cell with more than three live neighbours dies, as if by over-population.
-		//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 		var neighbours = [];
 		var thisCell = { 'row': $scope.row, 'column': $scope.column };
+		/*$scope.idString = function(){
+			return $scope.row.toString() + $scope.column.toString();
+		}*/
+		$scope.idString = $scope.row.toString() + $scope.column.toString();
 		$scope.statusClass = "dead";
+
+
+		//console.log('35', $scope.changefunc);
 
       	$scope.$on('checkSurroundingCells', function(){
       		//check population list
       		//does this cell have the requisite number of neighbouring cells to populate
+
       		
-      		//
-      		$scope.amIAlive();
+
+			//var intersect = neighbours.filter(function(n) {
+			//                   return $scope.population.indexOf(n) != -1
+			//	               });
+      		var localPop = angular.copy($scope.population);
+	      	
+	      		//populate with the neighbours
+	      		//row +-1, column +-1
+	      		
+	  			for (var i = $scope.row - 1; i >= $scope.row + 1; i++) {
+	  				for (var j = $scope.column - 1; j >= $scope.column + 1; j++) {
+	      				 neighbours.push({
+	      				 	'row': i,
+	      				 	'column': j
+	      				 });
+	      			};
+	  			};
+	      		
+	      	
+
+	      	console.log('neighbours', neighbours);
+	      	console.log('localPop', localPop);
+      		var intersect = _.intersection(neighbours, localPop);
+      		console.log(intersect);
+			if(intersect.length > 0){
+
+				if((intersect.length < 2 || intersect.length > 3) && $scope.statusClass == "alive"){
+					$scope.setDead();
+				}
+				else if(intersect.length == 3 && $scope.statusClass == "dead") {
+					$scope.setAlive();
+				}
+			}
+			
+
+      		
+      		
       	});
       	$scope.changeStatus = function(){
       		if($scope.statusClass == "dead") {
-      			$scope.statusClass = "alive";
+      			$scope.setAlive();
       		}
       		else if($scope.statusClass == "alive") {
-      			$scope.statusClass = "dead";
+      			$scope.setDead();
       		}
       	}
 
-      	$scope.amIAlive = function(){
+      	/*$scope.amIAlive = function(){
       		
       		//do the checks above - array intersect of population and neighbours
       		//add to population if not there and alive
@@ -55,37 +93,24 @@ angular.module('gameoflifeChallengeApp')
       		//return the appropriate css class
 
       		return 'dead';
-      	}
+      	}*/
 
       	$scope.setAlive = function(){
       		//if this is from the initial form setup, set as alive form class
       		$scope.statusClass = "alive";
-      		//add to population using angular filter
-      		//
-      		$scope.changefunc(thisCell, true);
+      		//add to population
+      		$scope.changeFunc({'item': thisCell, 'addOrRemove':true});
+      		
       	}
       	$scope.setDead = function(){
       		//if this is from the initial form setup, set as alive form class
       		$scope.statusClass = "dead";
       		//remove from population using angular filter
-      		//var filtered = $filter("filter")($scope.population, thisCell, true);
-      		$scope.changefunc(thisCell, false);
+      		$scope.changeFunc({'item': thisCell, 'addOrRemove':false});
+      		
       	}
 
-      	if (neighbours === []){
-      		//populate with the neighbours
-      		//row +-1, column +-1
-      		
-  			for (var i = row - 1; i >= row + 1; i++) {
-  				for (var j = column - 1; j >= column + 1; j++) {
-      				 neighbours.push({
-      				 	'row': i,
-      				 	'column': j
-      				 });
-      			};
-  			};
-      		
-      	}
+      	
 
 
       }
